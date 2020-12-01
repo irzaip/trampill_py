c824b2e14a1e101fa27c2428e72561df1ed619fd
machineska <alternative.xen@gmail.com>
2020-12-01 08:14:25 +0700

    add server config

--- trampillpy/settings.py
+++ trampillpy/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/trampil/neo.trampill.com/static/'
 
  
 STATICFILES_DIRS = [
