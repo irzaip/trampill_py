2cbeda1a15123f6e0767d85f1065fb1dbcf46a65
Irza Pulungan <irzaip@gmail.com>
2021-10-23 07:46:17 +0700

    Fix 2nd timedelta

--- trampillpy/settings.py
+++ trampillpy/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
-import timedelta
+from datetime import timedelta
 import environ
 
 from pathlib import Path
