efa7c66675543727e87c080767d88e7f122c0cdd
Irza Pulungan <irzaip@gmail.com>
2021-10-23 07:49:25 +0700

    Fix 3d timedelta

--- trampillpy/settings.py
+++ trampillpy/settings.py
@@ -188,7 +188,7 @@ SIMPLE_JWT = {
     'UPDATE_LAST_LOGIN': False,
 
     'ALGORITHM': 'HS256',
-    'SIGNING_KEY': settings.SECRET_KEY,
+    'SIGNING_KEY': SECRET_KEY,
     'VERIFYING_KEY': None,
     'AUDIENCE': None,
     'ISSUER': None,
